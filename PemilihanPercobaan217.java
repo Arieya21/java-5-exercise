1ec0ff91c597323c693d9b1c29a63f88c4c3ebee
Your Name <arieyacandrap21.com>
2023-09-27 15:57:01 +0700

    PemilihanPercobaan217

--- PemilihanPercobaan217.java
+++ PemilihanPercobaan217.java
@@ -4,7 +4,39 @@ public class PemilihanPercobaan217{
     public static void main(String[] args) {
         Scanner input17 = new Scanner(System.in);
 
-        System.out.print("Nilai uas    :");
-        float uas = input60.nextFloat();
+        System.out.print("Nilai uas   : ");
+        float uas = input17.nextFloat();
+        System.out.print("Nilai uts   : ");
+        float uts = input17.nextFloat();
+        System.out.print("Nilai kuis  : ");
+        float kuis = input17.nextFloat();
+        System.out.print("Nilai Tugas   : ");
+        float tugas = input17.nextFloat();
+
+        float total = (uas * 0.40f) + (uts * 0.30f) + (kuis * 0.1f) + (tugas * 0.2f);
+        System.out.println("Nilai Akhir \t\t:" + total);
+
+        if(total >= 81){
+            System.out.println("Anda mendapat Nilai huruf A");
+        }
+        else if(total >= 74){
+            System.out.println("Anda mendapatkan Nilai huruf B+");
+        }
+        else if(total >= 66){
+            System.out.println("Anda mendapatkan Nilai huruf B+");
+        }
+        else if(total >= 61){
+            System.out.println("Anda mendapatkan Nilai C+");
+        }
+        else if(total >= 51){
+            System.out.println("Anda mendapatkan Nilai C");
+        }
+        else if(total >= 38){
+            System.out.println("Anda mendapatkan Nilai D");
+        }
+        else if(total >= 0){
+            System.out.println("Anda mendapatkan Nilai huruf E");
+        }
+        input17.close();
     }
 }
